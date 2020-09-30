51106c30d996118e1aedfb3c272e053fda659a88
Alexandre conil <45951765+cnl-alexandre@users.noreply.github.com>
2020-09-30 15:39:54 +0200

    Evolution : Home + Paiement accepté page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -408,33 +408,7 @@
         </div>
         <div class="row mt-3">
             <div class="col-lg-5 mb-5" data-aos="fade-up" data-aos-delay="100">
-                <form action="#" method="post">
-                    <div class="form-group row">
-                        <div class="col-md-6 mb-4 mb-lg-0">
-                            <input type="text" class="form-control" placeholder="First name">
-                        </div>
-                        <div class="col-md-6">
-                            <input type="text" class="form-control" placeholder="First name">
-                        </div>
-                    </div>
-
-                    <div class="form-group row">
-                        <div class="col-md-12">
-                            <input type="text" class="form-control" placeholder="Email address">
-                        </div>
-                    </div>
-
-                    <div class="form-group row">
-                        <div class="col-md-12">
-                            <textarea name="" id="" class="form-control" placeholder="Write your message." cols="30" rows="10"></textarea>
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <div class="col-md-6 ml-auto">
-                            <input type="submit" class="btn btn-block btn-primary text-white py-3 px-5" value="Send Message">
-                        </div>
-                    </div>
-                </form>
+                <img src="{{ url('/medias/img/paiement-stripe.jpg') }}" alt="Image" class="img-fluid rounded-circle">
             </div>
             <div class="col-lg-5 ml-auto" data-aos="fade-up" data-aos-delay="200">
                 <h2 class="text-black">Laissez-nous un message</h2>
--- resources/views/paiement-accepte.blade.php
+++ resources/views/paiement-accepte.blade.php
@@ -4,11 +4,34 @@
 
 @section('content')
 
-<div class="notification">
-    <img class="logo" src="assets/logo.png" alt="Logo entreprise">
-    <h1 class="title">Création du site en cours</h1>
-    <p class="text">Bonjour, nous sommes actuellement en cours de création du site internet, celui-ci devrait être mis en ligne dans les prochains jours.</p>
-    <img class="illustration" src="assets/illustration.svg" alt="Illustration site maintenance">
-</div>
+<section class="site-section bg-light" id="accepted-section" style="height: 100vh;">
+    <div class="container">
+        <div class="row mb-5 justify-content-center text-center">
+            <div class="col-md-7">
+                <div class="block-heading-1" data-aos="fade-up" data-aos-delay="">
+                    <h2>Paiement accepté</h2>
+                </div>
+            </div>
+        </div>
+        <div class="row mb-5 d-flex align-items-center">
+            <div class="col-md-6 mb-4 mb-lg-0 col-lg-4" data-aos="fade-up" data-aos-delay="" style="margin-left: auto;margin-right: 0;">
+                <div class="text-center">
+                    <img src="{{ url('/medias/img/checked.svg') }}" alt="Paiement accepté" class="img-fluid rounded-circle" width="200px;" >
+                </div>
+            </div>
+            <div class="col-md-6 mb-4 mb-lg-0 col-lg-4" data-aos="fade-up" data-aos-delay="100" style="margin-left: 0;margin-right: auto;">
+                <div class="pricing">
+                    <h4 class="text-center text-black mb-5">Réservation validée !</h4>
+                    <p class="text-center text-black mb-4">Un mail de confirmation vous a été envoyé suite à votre réservation sur notre site Bullao.</p>
+                    <p class="text-center text-black mb-5">Nous vous remerçions pour votre confiance !</p>
+                    <p class="text-center">
+                        <a href="{{ url('/') }}" class="btn btn-primary btn-md text-white">Terminer</a>
+                    </p>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
 
 @endsection
