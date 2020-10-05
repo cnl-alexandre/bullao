957c8690bad3b27ec022def4b27c535e7d4632ef
Alexandre conil <45951765+cnl-alexandre@users.noreply.github.com>
2020-10-05 16:12:03 +0200

    Correction : Footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -5,7 +5,10 @@
                 <div class="row">
                     <div class="col-md-8">
                         <h2 class="footer-heading mb-4">A propos</h2>
-                        <p>Bullao propose des locations de spa et jacuzzi à domicile sur le secteur de Paris et Marne-la-Vallée.</p>
+                        <p>
+                            Bullao propose des locations de spa et jacuzzi à domicile sur le secteur de Paris et Marne-la-Vallée.
+                            <br>Bullao est un nom commercial utilisé (et déposé à l'INPI) par Grégoire Conil.
+                        </p>
                     </div>
                   <div class="col-md-4 ml-auto">
                       <h2 class="footer-heading mb-4">Rubriques</h2>
@@ -20,17 +23,6 @@
                 </div>
             </div>
             <div class="col-md-4 ml-auto">
-                <!-- <div class="mb-5">
-                    <h2 class="footer-heading mb-4">S'inscrire à la Newsletter</h2>
-                    <form action="#" method="post" class="footer-suscribe-form">
-                        <div class="input-group mb-3">
-                            <input type="text" class="form-control border-secondary text-white bg-transparent" placeholder="Enter Email" aria-label="Enter Email" aria-describedby="button-addon2">
-                            <div class="input-group-append">
-                                <button class="btn btn-primary text-white" type="button" id="button-addon2">S'inscrire</button>
-                            </div>
-                        </div>
-                    </form>
-                </div> -->
                 <h2 class="footer-heading mb-4">Suivez-nous !</h2>
                 <a href="#about-section" class="smoothscroll pl-0 pr-3"><span class="icon-facebook"></span></a>
                 <a href="#" class="pl-3 pr-3"><span class="icon-instagram"></span></a>
@@ -40,9 +32,7 @@
             <div class="col-md-12">
                 <div class="border-top pt-3">
                     <p class="copyright"><small>
-                    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
                     Copyright &copy;{{ date('Y') }} Tous droits réservés - Bullao - Grégoire Conil
-                    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
                     </small></p>
                 </div>
             </div>
