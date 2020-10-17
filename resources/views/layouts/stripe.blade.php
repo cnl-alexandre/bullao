935b6d7ef1aee1e91345d7473461b88ac7760aad
Alexandre conil <45951765+cnl-alexandre@users.noreply.github.com>
2020-10-17 22:47:45 +0200

    Evolution : Add Analytics + Hotjar

--- resources/views/layouts/stripe.blade.php
+++ resources/views/layouts/stripe.blade.php
@@ -4,6 +4,7 @@
         <title>@yield('pageTitle')</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="shortcut icon" type="image/jpg" href="{{ url('/medias/img/logo-compact.png') }}"/>
 
         <!-- DESACTIVATION INDEXATION -->
         <meta name="robots" content="noindex">
--- resources/views/layouts/tunnel.blade.php
+++ resources/views/layouts/tunnel.blade.php
@@ -25,6 +25,17 @@
         <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
 
         <script src="https://js.stripe.com/v3/"></script>
+
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-180769448-1"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-180769448-1');
+        </script>
+        <!-- Fin Google Analytics -->
     </head>
     <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
         <div class="site-wrap">
@@ -60,4 +71,17 @@
     </body>
 
     <script async type="text/javascript" src="https://userlike-cdn-widgets.s3-eu-west-1.amazonaws.com/dbc2b88096ae40eb9710cd74e16ebf84715c7466079d48dab279d2a834f4aa12.js"></script>
+
+    <!-- Hotjar Tracking Code for www.bullao.fr -->
+    <script>
+        (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:2040877,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+    </script>
+    <!-- Fin Hotjar -->
 </html>
