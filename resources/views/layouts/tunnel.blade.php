de949abd28cb3de2e0ba3187983eef841c149ccd
Alexandre conil <45951765+cnl-alexandre@users.noreply.github.com>
2020-10-18 13:51:47 +0200

    Evolution : Condition local/production GAnalytics + Hotjar

--- resources/views/layouts/tunnel.blade.php
+++ resources/views/layouts/tunnel.blade.php
@@ -6,10 +6,12 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="shortcut icon" type="image/jpg" href="{{ url('/medias/img/favicon.webp') }}"/>
 
-        <!-- DESACTIVATION INDEXATION -->
-        <meta name="robots" content="noindex">
-        <meta name="googlebot" content="noindex">
-        <!-- FIN -->
+        @if(env('APP_ENV') == "local")
+            <!-- DESACTIVATION INDEXATION -->
+            <meta name="robots" content="noindex">
+            <meta name="googlebot" content="noindex">
+            <!-- FIN -->
+        @endif
 
         <link rel="stylesheet" href="{{ url('/fonts/icomoon/style.css') }}">
 
@@ -27,16 +29,18 @@
 
         <script src="https://js.stripe.com/v3/"></script>
 
-        <!-- Global site tag (gtag.js) - Google Analytics -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-180769448-1"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
+        @if(env('APP_ENV') == "production")
+            <!-- Global site tag (gtag.js) - Google Analytics -->
+            <script async src="https://www.googletagmanager.com/gtag/js?id=UA-180769448-1"></script>
+            <script>
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
 
-          gtag('config', 'UA-180769448-1');
-        </script>
-        <!-- Fin Google Analytics -->
+              gtag('config', 'UA-180769448-1');
+            </script>
+            <!-- Fin Google Analytics -->
+        @endif
     </head>
     <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
         <div class="site-wrap">
@@ -73,16 +77,18 @@
 
     <script async type="text/javascript" src="https://userlike-cdn-widgets.s3-eu-west-1.amazonaws.com/dbc2b88096ae40eb9710cd74e16ebf84715c7466079d48dab279d2a834f4aa12.js"></script>
 
-    <!-- Hotjar Tracking Code for www.bullao.fr -->
-    <script>
-        (function(h,o,t,j,a,r){
-            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-            h._hjSettings={hjid:2040877,hjsv:6};
-            a=o.getElementsByTagName('head')[0];
-            r=o.createElement('script');r.async=1;
-            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-            a.appendChild(r);
-        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-    </script>
-    <!-- Fin Hotjar -->
+    @if(env('APP_ENV') == "production")
+        <!-- Hotjar Tracking Code for www.bullao.fr -->
+        <script>
+            (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:2040877,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+        </script>
+        <!-- Fin Hotjar -->
+    @endif
 </html>
