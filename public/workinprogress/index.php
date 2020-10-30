db5df6098347ce59339d44db83b12fa51ac81dee
Alexandre conil <45951765+cnl-alexandre@users.noreply.github.com>
2020-10-30 22:23:11 +0100

    Evolution : WorkInProgress page

---
+++ public/workinprogress/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta content="text/html charset=iso-8859-1">
+  <title>Bullao - Site en création</title>
+  <link rel="stylesheet" href="assets/style.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+  <link rel="shortcut icon" type="image/jpg" href="assets/logo.png"/>
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-180769448-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-180769448-1');
+</script>
+</head>
+<body>
+  <section class="englobe">
+    <section class="page">
+
+      <div class="notification">
+        <img class="logo" src="assets/logo.png" alt="Logo entreprise">
+        <h1 class="title">Création du site en cours</h1>
+        <p class="text">Bonjour, nous sommes actuellement en cours de création du site internet, celui-ci devrait être posté dans les prochains jours.</p>
+        <img class="illustration" src="assets/illustration.svg" alt="Illustration site maintenance">
+      </div>
+
+    </section>
+  </section>
+</body>
+<script async type="text/javascript" src="https://userlike-cdn-widgets.s3-eu-west-1.amazonaws.com/dbc2b88096ae40eb9710cd74e16ebf84715c7466079d48dab279d2a834f4aa12.js"></script>
+<!-- Hotjar Tracking Code for www.bullao.fr -->
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:2040877,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
+</html>
